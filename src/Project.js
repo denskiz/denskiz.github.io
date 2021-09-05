f2f4e4b24156fc71b52ad245557b9d099641cf96
dennis <dennis@debian-BULLSEYE-live-builder-AMD64>
2021-09-05 12:26:50 +1000

    F

--- src/Project.js
+++ src/Project.js
@@ -2,16 +2,26 @@ const Project = (props) => {
   return (
     <div>
       <div className="item">
-        <a href={props.link} target="_blank">
+        <a href={props.link} target="_blank" rel="noopener">
           <img src={props.image} alt="Project" />
         </a>
-        <a href={props.link} target="_blank" className="btn-light">
+        <a
+          href={props.link}
+          target="_blank"
+          className="btn-light"
+          rel="noopener"
+        >
           <i className="fas fa-eye"></i>
           {props.title} <br />
           {props.description} <br />
           {props.built}
         </a>
-        <a href={props.github} target="_blank" className="btn-dark">
+        <a
+          href={props.github}
+          target="_blank"
+          className="btn-dark"
+          rel="noopener"
+        >
           <i className="fab fa-github"></i> Github
         </a>
       </div>
