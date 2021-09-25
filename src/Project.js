67fc861d3315fd6e6f16f1be91dbda5c99f28a90
denskiz <dennisent@protonmail.com>
2021-09-25 11:08:20 +1000

    d

--- src/Project.js
+++ src/Project.js
@@ -1,30 +1,28 @@
 const Project = (props) => {
   return (
     <div>
-      <div className="item">
-        <a href={props.link} target="_blank" rel="noreferrer">
-          <img src={props.image} alt="Project" />
-        </a>
-        <a
-          href={props.link}
-          target="_blank"
-          className="btn-light"
-          rel="noreferrer"
-        >
-          <i className="fas fa-eye"></i>
-          {props.title} <br />
-          {props.description} <br />
-          {props.built}
-        </a>
-        <a
-          href={props.github}
-          target="_blank"
-          className="btn-dark"
-          rel="noreferrer"
-        >
-          Github
-        </a>
-      </div>
+      <a href={props.link} target="_blank" rel="noreferrer">
+        <img src={props.image} alt="Project" />
+      </a>
+      <a
+        href={props.link}
+        target="_blank"
+        className="btn-light"
+        rel="noreferrer"
+      >
+        <i className="fas fa-eye"></i>
+        {props.title} <br />
+        {props.description} <br />
+        {props.built}
+      </a>
+      <a
+        href={props.github}
+        target="_blank"
+        className="btn-dark"
+        rel="noreferrer"
+      >
+        Github
+      </a>
     </div>
   );
 };
